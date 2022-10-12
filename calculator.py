42e19bfe62cf6348c93539380991270eb92c81c2
Rajani44 <111074721+Rajani44@users.noreply.github.com>
2022-10-12 19:25:49 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,46 @@
+count=0
+while True:
+       num1 = int(input("enter first number: "))
+       num2 = int(input("enter second number: "))
+       operator = str(input("enter which operation you want:"))
+       if operator == "+":
+           print("result is:", (num1 + num2))
+           count+=1
+       elif operator == "-":
+           print("result is:", (num1 - num2))
+           count+=1
+       elif operator == "*":
+           print("result is:", (num1 * num2))
+           count+=1
+       elif operator == "/":
+           print("result is:", (num1 / num2))
+           count+=1
+       r=input('do you want to continue the code:')
+       if r=="exit":
+           # 1.append([input(operator)])
+           print(count)
+           break
+       else:
+           continue
+
+          
+   OUTPUT:
+enter first number: 4
+enter second number: 4
+enter which operation you want:*
+result is: 16
+do you want to continue the code:y
+enter first number: 2
+enter second number: 8
+enter which operation you want:+
+result is: 10
+do you want to continue the code:y
+enter first number: 6
+enter second number: 4
+enter which operation you want:-
+result is: 2
+do you want to continue the code:exit
+3
+
+ 
+    
